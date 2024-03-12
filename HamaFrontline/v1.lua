342444c260f7d3bf46716db6e936ee1a3c741932
mberthold <max-berthold@gmx.net>
2024-03-12 23:09:22 +0100

    Updated Sitzkrieg.miz
    Added new ideas to Concept.txt
    Added initial commands to make red and blue units move around
    Added suppression module for red units (so they run away an take cover)

--- HamaFrontline/v1.lua
+++ HamaFrontline/v1.lua
@@ -23,11 +23,30 @@ for n, x in ipairs(templates_red_ground_log) do
     table.insert(templates_red_main, x)
 end
 
+local function set_supression_red(group)
+    MESSAGE:New( "Rowing with one hand!", 35, "INFO" ):ToAll()
+    if group then
+        
+        MESSAGE:New( group.GroupName, 35, "INFO" ):ToAll()
+        local supression = SUPPRESSION:New(group)
+        supression:Takecover(true)
+        supression:Fallback(true)
+        supression:SetFallbackDistance(200)
+        supression:__Start(5)
+        MESSAGE:New( 'Did the suppression... hopefully.', 35, "INFO" ):ToAll()
+    else 
+        if group == nil then
+            MESSAGE:New( 'group is nil!', 35, "INFO" ):ToAll()
+        end
+    end
+end
+
 Spawn_Red_Light = SPAWN:New(templates_red_ground_log[1])
     :InitLimit(50, 0)
-    :InitRandomizeRoute( 1, 1, 5000 )
+    :InitRandomizeRoute( 1, 1, 3000 )
     :InitRandomizeTemplate( templates_red_main )
-    :SpawnScheduled(5, .5)
+    :OnSpawnGroup(set_supression_red)
+    :SpawnScheduled(60, .5)
 
 -- ################### --
 
@@ -54,7 +73,7 @@ end
 
 Spawn_Blue_Light = SPAWN:New(templates_blue_ground_log[1])
     :InitLimit(50, 0)
-    :InitRandomizeRoute( 1, 1, 5000 )
+    :InitRandomizeRoute( 1, 1, 3000 )
     :InitRandomizeTemplate( templates_blue_main )
     :SpawnScheduled(5, .5)
 
